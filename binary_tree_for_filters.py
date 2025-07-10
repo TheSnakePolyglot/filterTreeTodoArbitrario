32d6f40f4017fda3e39c8bded21ee77ee25aa0b5
matugong <mg262004@gmail.com>
2025-07-10 15:37:46 -0300

    Busco donde terminan los parentesis para poder agregar al arbol La idea dsp es hacer lo mismo para lo que viene despues de esta busqueda y tambien para las busquedas de adentro

--- binary_tree_for_filters.py
+++ binary_tree_for_filters.py
@@ -23,5 +23,18 @@ def generate_ABB_from_filter_string(filter_string_split: list[str]) -> ABBenPy:
     This function parses strings like ( ( ( Prop < 15 ) AND Booly ) OR ( Num == 5 ) ) AND ( OtherBool OR ( realy > 0 ) ) that have already been .split()
     """
     
+    count_paren: int = 1 if filter_string_split[0] == "(" else 0
+    
+    i: int = 0
+    while ((count_paren != 0) and (i < len(filter_string_split))):
+        
+        if (filter_string_split[i] == "("):
+            count_paren += 1
+        elif (filter_string_split[i] == ")"):
+            count_paren -= 1
+        
+        i += 1
+    
+    
     
         
