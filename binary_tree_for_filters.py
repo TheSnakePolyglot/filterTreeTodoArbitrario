51af40916841d66448c2990b3721324848c557da
matugong <mg262004@gmail.com>
2025-07-10 12:13:24 -0300

    1er commit. Funcion 'crear()', no le paso parametros. Esto se lo dejo a la funcion 'generar a partir de string'

---
+++ binary_tree_for_filters.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+import typing
+
+
+class ABBenPy:
+    
+    def __init__(self):
+        self.value: str = ""
+        self.izq: typing.Self
+        self.der: typing.Self
+        
+    
+    # def generate_ABB_from
+        
