3c123346939b988950dbe9a3c62e79cfa33e986f
matugong <mg262004@gmail.com>
2025-07-10 15:19:15 -0300

    Decidi un formato para las inputs de las strings, que para facilitarme todo les voy a hacer un .split() Esto no es un ABB al final, es solo un arbol binario, porque donde se insertar no se dice solo por el valor, le tengo que decir explciito Esto me da error de syntax, no se por q

--- binary_tree_for_filters.py
+++ binary_tree_for_filters.py
@@ -4,16 +4,24 @@ import typing
 
 class ABBenPy:
     
-    def __init__(self):
-        self.value: str = ""
+    def __init__(self, value: str):
+        self.value: str = value
         self.izq: typing.Optional[typing.Self] = None
         self.der: typing.Optional[typing.Self] = None
     
     
-    def insertar(self, valor: str):
-        pass
+    def insertar(self, valor: str, left: bool):
+        if left:
+            self.izq = ABBenPy(valor)
+        else:
+            self.der = ABBenPy(valor)
         
     
     
-def generate_ABB_from_filter_string(filter_string: str)
+def generate_ABB_from_filter_string(filter_string_split: list[str]) -> ABBenPy:
+    """ 
+    This function parses strings like ( ( ( Prop < 15 ) AND Booly ) OR ( Num == 5 ) ) AND ( OtherBool OR ( realy > 0 ) ) that have already been .split()
+    """
+    
+    
         
