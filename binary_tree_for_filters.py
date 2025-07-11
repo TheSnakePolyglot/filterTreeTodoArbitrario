7139140097b7a38a7b0a99342f2d4afa3a60f060
MatuGong <mg262004@gmail.com>
2025-07-11 09:19:29 -0300

    1er intento con esta funcion. Creo que la logica esta bien de la ultima parte

--- binary_tree_for_filters.py
+++ binary_tree_for_filters.py
@@ -27,32 +27,45 @@ class ABBenPy:
     
     def imprimir_arbol_binario(self, lvl: int):
         print(" " * lvl, self.value)
-        if self.izq != None:
-            self.izq.imprimir_arbol_binario(lvl + 1)
+
+        print(" " * lvl, "r: ")
         if self.der != None:
             self.der.imprimir_arbol_binario(lvl + 1)
+        print(" " * lvl, "l:")
+        if self.izq != None:
+            self.izq.imprimir_arbol_binario(lvl + 1)
     
     
-def generate_ABB_from_filter_string(filter_string_split: list[str], arbol_hasta_ahora: ABBenPy) -> ABBenPy:
+def generate_ABB_from_filter_string(filter_string_split: list[str], arbol_hasta_ahora: ABBenPy, deQueLado: bool):
     """ 
     This function parses strings like ( ( ( Prop < 15 ) AND Booly ) OR ( Num == 5 ) ) AND ( OtherBool OR ( realy > 0 ) ) that have already been .split()
     """
-    
-    count_paren: int = 1 if filter_string_split[0] == "(" else 0
-    
-    i: int = 0
-    while ((count_paren != 0) and (i < len(filter_string_split))):
+    if filter_string_split[0] == "(":
+        count_paren: int = 1
         
-        if (filter_string_split[i] == "("):
-            count_paren += 1
-        elif (filter_string_split[i] == ")"):
-            count_paren -= 1
+        i: int = 0
+        while ((count_paren != 0) and (i < len(filter_string_split))):
+            
+            if (filter_string_split[i] == "("):
+                count_paren += 1
+            elif (filter_string_split[i] == ")"):
+                count_paren -= 1
+            
+            i += 1
         
-        i += 1
+        # TODO: Ver que onda con los punteros aca, como puedo agregarle al arbol y al mismo tiempo pasar un nuevo arbol
+        
+        arbol_hasta_ahora.insertar(filter_string_split[i], deQueLado)
+
+        generate_ABB_from_filter_string(filter_string_split[1: i], arbol_hasta_ahora.izq, True)
+        generate_ABB_from_filter_string(filter_string_split[i: len(filter_string_split)], arbol_hasta_ahora.der, False)
     
-    # TODO: Ver que onda con los punteros aca, como puedo agregarle al arbol y al mismo tiempo pasar un nuevo arbol
-    
-    return 
+    else:
+        print("FINAL")
+        print(filter_string_split)
+        arbol_hasta_ahora.insertar(filter_string_split[1], True)
+        arbol_hasta_ahora.izq.insertar(filter_string_split[0], True)
+        arbol_hasta_ahora.der.insertar(filter_string_split[2], True)
     
 
 
@@ -62,11 +75,17 @@ if __name__ == "__main__":
     miArbol.insertar("je", True)
     miArbol.insertar("what", False)
     
+    miArbol.izq.insertar("yea", True)
     # nuevoarbol = ABBenPy("no")
     # otroarbolito = ABBenPy("der")
     
     # nuevoarbol.insertar(miArbol, True)
     
-    
     miArbol.imprimir_arbol_binario(0)
+
+
+    arbolGenerado = ABBenPy()
+    miFiltro = "( ( ( Prop < 15 ) AND Booly ) OR ( Num == 5 ) ) AND ( OtherBool OR ( realy > 0 ) )"
+    generate_ABB_from_filter_string(miFiltro.split(), arbolGenerado, True)
+    arbolGenerado.imprimir_arbol_binario(0)
     
