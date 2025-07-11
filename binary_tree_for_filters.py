aef856eac523d6506d36b0fbf0aa2bc1579a2968
MatuGong <mg262004@gmail.com>
2025-07-11 15:05:29 -0300

    Aclaracion sobre la estructura del ABB generado

--- binary_tree_for_filters.py
+++ binary_tree_for_filters.py
@@ -38,14 +38,12 @@ def generate_ABB_from_filter_string(filter_string_split: list[str], arbol_hasta_
     """ 
     This function parses strings like ( ( ( ( Prop ) < ( 15 ) ) AND ( Booly ) ) OR ( ( Num ) == ( 5 ) ) ) AND ( ( OtherBool ) OR ( ( realy ) > ( 0 ) ) ) that have already been .split()
     Easier example: ( ( This ) > ( 0 ) ) OR ( SomeBool )
+
+    Por como esta escrito "insertar()", esta funcion siempre genera hojas que apunten a ABBs vacios (o sea que self.esVacio() == True) en vez de que las hojas apunten a NULL (None)
     """
 
     if filter_string_split[0] == "(":
         count_paren: int = 1
-        
-
-        print(filter_string_split)
-
 
         i: int = 1
         while ((count_paren != 0) and (i < len(filter_string_split))):
@@ -57,51 +55,18 @@ def generate_ABB_from_filter_string(filter_string_split: list[str], arbol_hasta_
             
             i += 1
         
-
-        print(i)
-        print(filter_string_split[i])
-        print("---")
-
-
-        # TODO: Ver que onda con los punteros aca, como puedo agregarle al arbol y al mismo tiempo pasar un nuevo arbol
-        
         arbol_hasta_ahora.insertar(filter_string_split[i])
-        print("ins")
-        print(".....")
 
         generate_ABB_from_filter_string(filter_string_split[1: i-1], arbol_hasta_ahora.izq)
         generate_ABB_from_filter_string(filter_string_split[i+2: len(filter_string_split) - 1], arbol_hasta_ahora.der)
 
     else:
-        print("FINAL")
-        print(filter_string_split)
-
-        # if len(filter_string_split) == 3:
-        #     arbol_hasta_ahora.insertar(filter_string_split[1], True)
-        #     arbol_hasta_ahora.izq.insertar(filter_string_split[0], True)
-        #     arbol_hasta_ahora.der.insertar(filter_string_split[2], True)
-        # else:
         arbol_hasta_ahora.insertar(filter_string_split[0])
-        
-        print("listop")
-        print("------------")
+
     
 
 
 if __name__ == "__main__":
-    # miArbol = ABBenPy()
-    # miArbol.insertar("1", True)
-    # miArbol.insertar("je", True)
-    # miArbol.insertar("what", False)
-    
-    # miArbol.izq.insertar("yea", True)
-    # # nuevoarbol = ABBenPy("no")
-    # # otroarbolito = ABBenPy("der")
-    
-    # # nuevoarbol.insertar(miArbol, True)
-    
-    # miArbol.imprimir_arbol_binario(0)
-
 
     arbolGenerado = ABBenPy()
     miFiltro = "( ( ( ( Prop ) < ( 15 ) ) AND ( Booly ) ) OR ( ( Num ) == ( 5 ) ) ) AND ( ( OtherBool ) OR ( ( realy ) > ( 0 ) ) )"
